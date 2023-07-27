a1329f7b0b51b7d750cabc923c1237d7fd68d752
Jorge Luis Sepúlveda Abreu <86900623+JorgeL-SepulvedaA@users.noreply.github.com>
2023-07-26 22:31:16 -0400

    not the actual app whatsoever.

--- chrono-house/src/app/tab1/tab1.page.ts
+++ chrono-house/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
+// import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -8,24 +9,23 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class Tab1Page {
 
-  constructor(private camera: Camera) {}
+  // constructor(private camera: Camera) {}
 
-  takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    };
+  // takePicture() {
+  //   const options: CameraOptions = {
+  //     quality: 100,
+  //     destinationType: this.camera.DestinationType.DATA_URL,
+  //     encodingType: this.camera.EncodingType.JPEG,
+  //     mediaType: this.camera.MediaType.PICTURE
+  //   };
   
-    this.camera.getPicture(options).then((imageData) => {
-      // imageData is a base64 encoded string
-      let base64Image = 'data:image/jpeg;base64,' + imageData;
-      // Process the captured image as per your requirement
-    }, (err) => {
-      console.log(err);
-    });
-  }
-  
+  //   this.camera.getPicture(options).then((imageData) => {
+  //     // imageData is a base64 encoded string
+  //     let base64Image = 'data:image/jpeg;base64,' + imageData;
+  //     // Process the captured image as per your requirement
+  //   }, (err) => {
+  //     console.log(err);
+  //   });
+  // }
 
 }
